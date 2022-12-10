28985bdfcc2651c5693a921dbefa0335c2c3dc2c
Leonid <rockwil91@gmail.com>
2022-12-10 05:07:21 +0200

    Д/З 9 - Задача 64

--- task 0/Program.cs
+++ task 0/Program.cs
@@ -3,3 +3,22 @@
 
 N = 5 -> "5, 4, 3, 2, 1"
 N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"*/
+
+
+int n = GetInt("Введите число: ");
+Console.Write(NumbersDown(0, n));
+
+int GetInt (string msg)
+{
+    Console.WriteLine(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int NumbersDown(int a, int b)
+{
+    if (b <= a)
+        return a;
+    else
+        Console.Write(NumbersDown(a + 1, b) + ", ");
+    return a;
+}
